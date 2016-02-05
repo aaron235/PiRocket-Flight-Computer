28b53f9729cd6cd9e5f20bdbeb0465e07056503e
piriys <piriys@live.com>
2016-02-04 16:06:41 -0800

    Removed gps.py and Fixed some parsing
    Fixed date/time parse formatting
    Graph autoscaling both X and Y axis

--- code/gps_gui.py
+++ code/gps_gui.py
@@ -1,38 +1,37 @@
+import math
 import serial
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# TO DO: add error handling 
-
 class GPRMC:
     def __init__(self, args):
-        self.time = args[1].split('.')[0]
+        self.time = args[1][:2] + ':' + args[1][2:4] + ':' + args[1][4:6]
         self.nav = args[2]
         self.lat = args[3]
         self.lat_NS = args[4]
         self.long = args[5]
         self.long_EW = args[6]
-        self.speed = args[7]
+        self.speed = float(args[7]) 
         self.course = args[8]
-        self.date = args[9]
+        self.date = args[9][:2] + '/' + args[1][2:4] + '/' + args[1][4:6]
         self.var = args[10]
         self.var_EW = args[11]
         self.checksum = args[12]
 
-s = serial.Serial('COM5', 9600)
+s = serial.Serial('COM3', 9600)
 w = csv.writer(open('gps_logs.csv', 'w'), delimiter = ',', lineterminator = '\n')        
         
 def init():
-    ax.set_ylim(-1.1, 1.1)
+    ax.set_ylim(0, 1.1)
     ax.set_xlim(0, 10)
     del xdata[:]
     del ydata[:]
     line.set_data(xdata, ydata)
     return line,
 
-def data_gen(t=0):
+def data_gen(t = 0):
     while True:
         line = s.readline()
         args = line.split(',')
@@ -44,23 +43,28 @@ def data_gen(t=0):
             # write to csv
             w.writerow([data.time, data.date, data.lat + data.lat_NS, data.long + data.long_EW])  
             t += 1
-            yield data.speed, t
+            yield t, data.speed 
     
 fig, ax = plt.subplots()
-line, = ax.plot([], [], lw=2)
+line, = ax.plot([], [], lw = 2)
 ax.grid()
 xdata, ydata = [], []
 
 def run(data):
     # update the data
-    t, y = data
+    t, speed = data
     xdata.append(t)
-    ydata.append(y)
+    ydata.append(speed)
     xmin, xmax = ax.get_xlim()
-
+    speed_min, speed_max = ax.get_ylim()
+    
     if t >= xmax:
-        ax.set_xlim(xmin, 2*xmax)
+        ax.set_xlim(xmin, xmax + 10)
         ax.figure.canvas.draw()
+    if speed > speed_max:
+        ax.set_ylim(speed_min, speed + 0.1)
+        ax.figure.canvas.draw()
+        
     line.set_data(xdata, ydata)
 
     return line,
@@ -68,4 +72,10 @@ def run(data):
 
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
     repeat=False, init_func=init)
+
+plt.title('Speed vs Time')
+plt.xlabel('Time [Seconds]')
+plt.ylabel('Speed [Knots]')    
+fig.canvas.set_window_title('RocketPi') 
+plt.show()
     
