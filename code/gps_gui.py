640f655c7a3941e552e4746b065db09cba6451c5
piriys <piriys@live.com>
2016-02-04 17:53:25 -0800

    Parse fixes

--- code/gps_gui.py
+++ code/gps_gui.py
@@ -8,23 +8,35 @@ import matplotlib.animation as animation
 class GPRMC:
     def __init__(self, args):
         self.time = args[1][:2] + ':' + args[1][2:4] + ':' + args[1][4:6]
-        self.nav = args[2]
-        self.lat = args[3]
-        self.lat_NS = args[4]
-        self.long = args[5]
-        self.long_EW = args[6]
-        self.speed = float(args[7]) 
-        self.course = args[8]
         self.date = args[9][:2] + '/' + args[1][2:4] + '/' + args[1][4:6]
-        self.var = args[10]
-        self.var_EW = args[11]
+        self.nav = args[2]
+        
+        if self.nav == 'A': 
+            self.lat = float(args[3][:2]) + float(args[3][2:])
+            self.lat_NS = args[4]
+            self.long = float(args[5][:3]) + float(args[5][3:])
+            self.long_EW = args[6]
+            self.speed = float(args[7]) 
+            self.course = float(args[8])            
+            self.var = args[10] # Not available with Adafruit Ultimate GPS)
+            self.var_EW = args[11] # Not available with Adafruit Ultimate GPS)
+        else:
+            self.lat = ''
+            self.lat_NS = ''
+            self.long = ''
+            self.long_EW = ''
+            self.speed = '' 
+            self.course = ''            
+            self.var = ''
+            self.var_EW = ''
+            
         self.checksum = args[12]
 
 s = serial.Serial('COM3', 9600)
 w = csv.writer(open('gps_logs.csv', 'w'), delimiter = ',', lineterminator = '\n')        
         
 def init():
-    ax.set_ylim(0, 1.1)
+    ax.set_ylim(0, 10)
     ax.set_xlim(0, 10)
     del xdata[:]
     del ydata[:]
@@ -37,13 +49,16 @@ def data_gen(t = 0):
         args = line.split(',')
         if args[0] == '$GPRMC':
             data = GPRMC(args)
+            print line
             print 'Time:', data.time, 'Date:',  data.date, 'Status:', data.nav
             print 'Speed:', data.speed, 'Lat:', data.lat, data.lat_NS, 'Long:', data.long, data.long_EW
+            print 'Course:', data.course
             print '----------------'
+            
             # write to csv
-            w.writerow([data.time, data.date, data.lat + data.lat_NS, data.long + data.long_EW])  
+            w.writerow([data.time, data.date, data.lat, data.lat_NS, data.long, data.long_EW])  
             t += 1
-            yield t, data.speed 
+            yield t, data.speed
     
 fig, ax = plt.subplots()
 line, = ax.plot([], [], lw = 2)
@@ -62,7 +77,7 @@ def run(data):
         ax.set_xlim(xmin, xmax + 10)
         ax.figure.canvas.draw()
     if speed > speed_max:
-        ax.set_ylim(speed_min, speed + 0.1)
+        ax.set_ylim(speed_min, speed)
         ax.figure.canvas.draw()
         
     line.set_data(xdata, ydata)
