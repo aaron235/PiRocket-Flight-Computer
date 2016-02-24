6ab2b818787b020829a29a5d5d53a6b657f51d09
aaron235 <qwertyman159@gmail.com>
2016-02-23 17:48:41 -0800

    Added a functional accelerometer object to the sensorLoggers

--- code/sensorLoggers.py
+++ code/sensorLoggers.py
@@ -1,9 +1,56 @@
 import datetime
 import csv
 
-import Adafruit_BMP.BMP085 as BMP085
+import lib.Adafruit_Python_BMP.Adafruit_BMP.BMP085.BMP085 as BMP085
+import lib.Adafruit-Raspberry-Pi-Python-Code.Adafruit_ADS1x15.ADS1x15 as ADS1115
+import lib.Adafruit-Raspberry-Pi-Python-Code.Adafruit_ADS1x15.ADS1x15 as ADS1115
+import lib.Adafruit-Raspberry-Pi-Python-Code.Adafruit_ADS1x15.ADS1x15 as ADS1115
+
 # import Adafruit_ADXL.ADXL377 as ADXL377
 # import UltimateGPS
+
+
+class Barmoeter( object ):
+	def __init__( self, mode ):
+		self.sensor = BMP085( mode )
+
+
+class Accelerometer( object ):
+	def __init__( self ):
+		self.sensor = ADS1115()
+		# Our calibrated zero values for the accelerometer:
+		self.xZero = 1657.8712
+		self.yZero = 1660.7111
+		self.zZero = 1659.3377
+		# And, our calibrated scale factors for the accelerometer:
+		self.xScale = 7.126831
+		self.yScale = 7.286181
+		self.zScale = 7.271006
+		# Number of digits to round to for g-force data:
+		self.gForcePrecision = 5
+
+	def readX():
+		xRaw = self.sensor.readADCSingleEnded( channel=1, pga=4096, sps=200 )
+		xAdj = round( ( ( xRaw - self.xZero ) / self.xScale ), self.gForcePrecision )
+		return xAdj
+
+	def readY():
+		xRaw = self.sensor.readADCSingleEnded( channel=1, pga=4096, sps=200 )
+		xAdj = round( ( ( xRaw - self.xZero ) / self.xScale ), self.gForcePrecision )
+		return xAdj
+
+	def readZ():
+		xRaw = self.sensor.readADCSingleEnded( channel=1, pga=4096, sps=200 )
+		xAdj = round( ( ( xRaw - self.xZero ) / self.xScale ), self.gForcePrecision )
+		return xAdj
+
+
+class Gyroscope( object ):
+	def __init__( self ):
+
+
+class Magnetometer( object ):
+	def __init__( self ):
 
 
 def init():
