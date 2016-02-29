29f8bb612e5a26864a10a073c412ac16f7725497
aaron235 <qwertyman159@gmail.com>
2016-02-28 18:38:34 -0800

    Added a framework for logging data

--- code/main.py
+++ code/main.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
-# Our Adafruit I2C ADC library, for superior 16-bit resolution:
-from Adafruit_ADS1x15.Adafruit_ADS1x15 import ADS1x15
-# Our library to handle our BMP180 altimeter:
-# import Adafruit_BMP.BMP as BMP
 import sensorLoggers
-
+import json
 # Configuring our i2c addresses:
 ADDRESS_ADS1115 = 0x48
 ADDRESS_BMP180 = 0x77
 
 
-def main():
-	while True:
+def readData( accel, magnetometer, gyroscope, barometer, filename ):
+	outData = dict()
+	outData['accelX'] = accel.readX()
+	outData['accelY'] = accel.readY()
+	outData['accelZ'] = accel.readZ()
+	outData['magnetometerX'], outData['magnetometerY'], outData['magnetometerZ'] = magnetometer.read()
+	outData['gyroX'] = gyroscope.readX()
+	outData['gyroY'] = gyroscope.readY()
+	outData['gyroZ'] = gyroscope.readZ()
+	outData['pressure'] = barometer.read()
+
+	return outData
+
 
 
 
 # start er' up
 if ( __name__ == "__main__" ):
 	accel = sensorLoggers.Accelerometer()
-	main()
+	magnetometer = sensorLoggers.Magnetometer()
+	gyroscope = sensorLoggers.Gyroscope()
+	barometer = sensorLoggers.Barometer()
+	while True:
+		outData = readData( accel, magnetometer, gyroscope, barometer )
+		print( json.dumps( outData ) )
